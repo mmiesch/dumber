0b38ea5c58f28ddc090555bff6604d2fd4b3c52f
mmiesch <mark.s.miesch@gmail.com>
2022-02-11 09:44:27 -0700

    an alternative way to read the data using netCDF4

--- ReadData.py
+++ ReadData.py
@@ -5,8 +5,6 @@ Simple example of how to read and plot L1 MAG data from DSCOVR
 import numpy as np
 import matplotlib.pyplot as plt
 
-from scipy.io import netcdf
-
 #-----------------------------------------------------------------------------
 # open file for reading
 
@@ -15,12 +13,15 @@ dir = '/home/mark.miesch/data/DSCOVR/MAG/L1/'
 # Feb 5
 file = dir+'oe_mg1_dscovr_s20220205000000_e20220205235959_p20220206013755_pub.nc'
 
+#-----------------------------------------------------------------------------
 # old school netcdf3 approach seems to work:
+
+from scipy.io import netcdf
 
 dfile = netcdf.NetCDFFile(file,'r')
 
 tdata = dfile.variables['time']
-time = tdata[:].copy().astype('float')
+time = tdata[:].copy().astype('float') - tdata[0]
 
 bzdata = dfile.variables['bz_gsm']
 bz = bzdata[:].copy().astype('float')
@@ -28,13 +29,28 @@ bz = bzdata[:].copy().astype('float')
 dfile.close()
 
 #-----------------------------------------------------------------------------
+# an alternative, using the netcdf4 package
+
+from netCDF4 import Dataset
+
+rootgrp = Dataset(file, "r", format="NETCDF3")
+
+tvar = rootgrp.variables['time']
+time2 = tvar[:] - tvar[0]
+
+bzvar = rootgrp.variables['bz_gsm']
+bz2 = bzvar[:].copy()
+
+#-----------------------------------------------------------------------------
 
 i1 = 2000
-i2 = i1 + 400
+i2 = i1 + 50
 
 plt.plot(time[i1:i2],bz[i1:i2])
+plt.plot(time2[i1:i2],bz2[i1:i2],'bo')
 
 plt.show()
 
 #-----------------------------------------------------------------------------
 
+rootgrp.close()
