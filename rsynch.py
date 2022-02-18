8696ce74ac2e10ba91c480a8751ed90b872de320
mmiesch <mark.s.miesch@gmail.com>
2022-02-17 17:03:09 -0700

    dubug bartlett

--- rsynch.py
+++ rsynch.py
@@ -17,7 +17,7 @@ ytitle = 'signal (arbitrary units)'
 
 #----------------------------------------------------------------------------- 
 
-sam = 3
+sam = 4
 
 if sam == 1:
     dt1 = 1.01
@@ -35,7 +35,7 @@ elif sam == 3:
     ns=2000
     k=200
 elif sam == 4:
-    dt1 = 1.01
+    dt1 = 0.99
     offset = 0.0
     ns=2000
     k=200
@@ -68,23 +68,56 @@ b1 = np.cos(2*np.pi*k*time1/tmax)
 f = interp.interp1d(time1,b1,kind='linear')
 b2 = f(time2)
 
-b2check = np.cos(2*np.pi*k*time2/tmax)
+#-----------------------------------------------------------------------------
+# Bartlett filter
+
+b3 = np.zeros(len(time2))
+
+for j in np.arange(2,len(time2)-2):
+    tj = time2[j]
+    mm = (tj - time1[0])/dt1
+    m = int(np.rint(mm))
+
+    #print(f"x- {im-1} {tj - time1[im-1]}")
+    #print(f"xm {im}   {tj - time1[im]  }")
+    #print(f"x+ {im+1} {tj - time1[im+1]}")
+
+    x = tj - time1[m]
+    assert np.abs(x) <= 0.5
+
+    ell = m - int(2 - x)
+    n = m + int(2+x) 
+
+    nw = n - ell + 1
+    w = np.zeros(nw)
+
+    kk = 0
+    for k in np.arange(1,m-ell+1):
+        w[kk] = 1.0 - 0.5*(k+x)
+        kk += 1
+
+    w[kk] = 1.0 - 0.5 * x
+    kk += 1
+
+    for k in np.arange(1,n-m+1):
+        w[kk] = 1.0 - 0.5*(k-x)
+        kk += 1
+
+    w = w / np.sum(w)
+
+    b3[j] = np.sum(w*b1[ell:n+1])
+
+time3 = time2[2:len(time2)-2]
+b3 = b3[2:len(time2)-2]
 
 #-----------------------------------------------------------------------------
 
 plt.figure(figsize=(30,6))
 
-#plt.plot(time1,b1,'k-',linewidth=6)
-#plt.plot(time2,b2,color='#B1FB17',linewidth=4)
-
-#diff = b2check - b2
-#plt.plot(time2,diff,'b-',linewidth=4)
-
-#s = b2check + b2
-#plt.plot(time2,s,'b-',linewidth=4)
-
 plt.plot(time1,b1,'k-',linewidth=6)
-plt.plot(time2,b2,'y-',linewidth=3)
+#plt.plot(time2,b2,'y-',linewidth=3)
+
+plt.plot(time3,b3,'y-',linewidth=3)
 
 
 plt.xlabel(xtitle)
